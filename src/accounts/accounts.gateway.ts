3878ba78429af3349b35cbbc76adfe72d37f3d3c
Mario Barrajon Silva <marb@ciklum.com>
2020-10-03 16:53:18 +0200

    feat(): Added websockets support with a gateway and Socket.io

--- src/accounts/accounts.gateway.ts
+++ src/accounts/accounts.gateway.ts
@@ -19,22 +19,22 @@ export class AccountsGateway implements OnGatewayConnection, OnGatewayDisconnect
   @WebSocketServer() server;
 
   async handleConnection(): Promise<void> {
-    console.info('[INFO] ===> Connection established by client');
+    console.log('[INFO] ===> Connection established by client');
     this.initExchangeRatePush();
   }
 
   async handleDisconnect(){
-    console.info('[INFO] ===> Connection closed by client');
+    console.log('[INFO] ===> Connection closed by client');
   }
 
   // @SubscribeMessage(BITCOIN_RATE_CHANNEL)
   // async handleEvent(client, message): Promise<void> {
-  //   console.info('[INFO] ===> Handling event');
+  //   console.log('[INFO] ===> Handling event');
   //   this.initExchangeRatePush();
   // }
 
   private initExchangeRatePush(): void {
-    console.info('[INFO] ===> Pushing new Bitcoin exchange rate');
+    console.log('[INFO] ===> Pushing new Bitcoin exchange rate');
     this.server.emit(BITCOIN_RATE_CHANNEL, this.accountsService.getExchangeRate());
 
     setTimeout(() => {
